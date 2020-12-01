a512d3f3eef3210f9d64b57e4a5a786ded060b42
JokerSchweizer <jiang19951028@hotmail.com>
2020-12-01 22:36:03 +0800

    test vector

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 int main(int argc, char const *argv[]) {
   cout<<"jiang"<<endl;
+
+  std::vector<int> v;
+  for (size_t i = 0; i < 10; i++) {
+    /* code */
+    v.push_back(i);
+  }
   return 0;
 }
